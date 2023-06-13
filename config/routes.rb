ad7f407edf7bb2c6a9c53c81569e266fa1989bbe
Miwako <you@example.com>
2023-06-13 08:19:21 +0000

    W

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-#root to: 'homes#top'
+#root to: "public/homes#top"
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
    sessions: "admin/sessions",
